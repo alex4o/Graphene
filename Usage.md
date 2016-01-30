577dfb432c25b1726160a94f781e85cbbc96d620
alex4o <mrabonin@gmail.com>
2016-01-30 22:50:42 +0200

    Done all preparing. Now let's start the real work.

---
+++ Usage.md
@@ -0,0 +1,8 @@
+Използване на графена:
+
+1. Super capacitors [ http://link.springer.com/article/10.1007/s12039-008-0002-7 ]
+2. Water distilation And Purification [ http://www.sciencedirect.com/science/article/pii/S0376738815001878 ]
+3. Touch screen [ http://onlinelibrary.wiley.com/doi/10.1002/adfm.201402547/abstract ]
+4. Speakers [ http://arxiv.org/ftp/arxiv/papers/1303/1303.2391.pdf ]
+5. Long-lasting lubricant [ http://phys.org/news/2014-10-graphene-long-lasting-lubricant.html ]
+6. Biomedical Applications [ http://www.sciencedirect.com/science/article/pii/S1369702113003015 ]
---
+++ src/webpack.config.js
@@ -0,0 +1,44 @@
+var autoprefixer	= require('autoprefixer');
+var precss			= require('precss');
+var lost 			= require('lost');
+var cssnext 		= require('postcss-cssnext');
+
+module.exports = {
+	entry: ["./main.js"],
+	output: {
+		filename: "./out/bundle.js",   
+	},
+	module: {
+		loaders: [
+			{ 
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					cacheDirectory: true,
+					plugins: ['transform-runtime'],
+					presets: ['es2015', 'stage-0', 'react'],
+				}
+
+			},
+			{
+				test:   /\.css$/,
+				loader: "style-loader!css-loader!postcss-loader"
+			},
+			{
+				test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+				loader: 'url-loader'
+			}
+		]
+	},
+	postcss: function () {
+		return {
+			defaults: [autoprefixer, precss, lost, cssnext],
+			cleaner:  [autoprefixer({ browsers: ['last 1 version'] })]
+		};
+	},
+	resolve: {
+		modulesDirectories: ["node_modules"],
+	}
+	//,plugins: [/*new EncodingPlugin('utf-8'),*/ new BomPlugin(true)]
+}
