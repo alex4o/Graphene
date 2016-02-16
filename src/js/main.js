b4eee886325fd789289c64905d0d088545d0d965
alex4o <mrabonin@gmail.com>
2016-02-16 21:41:25 +0200

    Added the videos and made one of them loop

--- src/js/main.js
+++ src/js/main.js
@@ -9,15 +9,31 @@ class App extends React.Component
 	constructor(props){
 		super(props)
 		this.state = {
-
+			src: "preintro.mp4"
 		}
+	}
+
+	componentDidMount(){
+		window.video = this.video
+		this.video.onended = (e) => {
+
+			this.setState({src: "intro.mp4"});
+		}
+		this.video.addEventListener("timeupdate", function () {
+    	    if(this.currentTime >= 5.0) {
+    	        this.currentTime = 4.0;
+    	    }
+    	});
+
 	}
 	
 	render(){
 		return (
 			<div>
 				<h1>Hello graphene !!</h1>
-				
+				<video controls autoPlay ref={(ref) => this.video = ref}>
+					<source src={this.state.src} type="video/mp4"/>
+				</video>
 
 			</div>
 			)
