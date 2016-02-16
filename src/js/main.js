4173b4f0c3a4fa4c5474341dd8b2b6e125a516ba
alex4o <mrabonin@gmail.com>
2016-02-16 18:49:05 +0200

    Some init text

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Katex from "./katex";
+import lodash from "lodash"; //because I can
+
+//import TabPanel from "./tabPanel.js";
+
+class App extends React.Component 
+{
+	constructor(props){
+		super(props)
+		this.state = {
+
+		}
+	}
+	
+	render(){
+		return (
+			<div>
+				<h1>Hello graphene!!!</h1>
+				
+
+			</div>
+			)
+	}
+
+
+	
+}
+
