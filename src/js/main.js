f16e703a45ba25bec241401d472569c44e891034
alex4o <mrabonin@gmail.com>
2016-02-17 15:43:18 +0200

    Added the canvas on witch the drawing will happen, added the paper.js library for the canvas.

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import lodash from "lodash"; //because I can
 
+import paper from "paper";
+window.paper = paper;
+
+require("../css/global.css");
+
 //import TabPanel from "./tabPanel.js";
 
 class App extends React.Component 
@@ -15,30 +20,43 @@ class App extends React.Component
 
 	componentDidMount(){
 		window.video = this.video
-		this.video.onended = (e) => {
+		paper.setup(this.canvas)
+		let planet = new paper.Raster("./mercury.png")
+		planet.position = paper.view.center;
+		this.video.addEventListener("ended", (e) => {
 
 			this.setState({src: "intro.mp4"});
-		}
-		this.video.addEventListener("timeupdate", function () {
-    	    if(this.currentTime >= 5.0) {
-    	        this.currentTime = 4.0;
-    	    }
-    	});
+		});
+
+		window.addEventListener("resize", (e) => {
+			this.canvas.width  = window.innerWidth;
+  			this.canvas.height = window.innerHeight;
+			paper.view.draw();
+		});
+
+		this.video.addEventListener("timeupdate", () => {
+
+			if(this.video.currentTime >= 9.0) {
+				this.video.currentTime = 8.0;
+			}
+		});
 
 	}
 	
 	render(){
 		return (
 			<div>
-				<h1>Hello graphene !!</h1>
 				<video controls autoPlay ref={(ref) => this.video = ref}>
 					<source src={this.state.src} type="video/mp4"/>
 				</video>
+				<canvas id="drawSurf" className="draw" ref={(ref) => this.canvas = ref}></canvas>
 
 			</div>
 			)
 	}
 }
+
+
 
 document.addEventListener( "DOMContentLoaded", () => {
 	ReactDOM.render(<App/>, document.getElementById('app-root'));
