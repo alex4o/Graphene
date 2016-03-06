0cfcee4730f083e44fb08938b0a11efd2d281ed5
alex4o <mrabonin@gmail.com>
2016-03-06 16:56:36 +0200

    Rewrote the whole thing, becouse react.js geting in my way

--- src/js/main.js
+++ src/js/main.js
@@ -1,141 +1,134 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import _ from "lodash"; //because I can
-window._ = _;
 import paper from "paper";
 import rx from "rx"
-window.rx = rx;
-
-import Vidoe from "./video";
-window.paper = paper;
-
+import Story from "./story"
+window.p = paper
 require("../css/global.css");
 
-var story = [
-	{
-		"scene": "intro",
-		"src": "intro.mp4",
-		"choice": {
-			"exsists": true,
-			"list": [
-				{ "show": "choice 1", "scene": "sc1"},
-				{ "show": "choice 2", "scene": "sc2"},
-				{ "show": "choice 4", "scene": "sc3"}
-			]
-		}
-	},	
-	{
-		"scene": "sc1",
-		"src": "sc1.mp4",
-		"choice": {
-			"exsists": false,
-			"list": []
-		}
-	},	
-	{
-		"scene": "sc2",
-		"src": "sc2.mp4",
-		"choice": {
-			"exsists": false,
-			"list": []
-		}
-	},	
-	{
-		"scene": "sc3",
-		"src": "sc3.mp4",
-		"choice": {
-			"exsists": false,
-			"list": []
-		}
-	}
-]
 
+let story = new Story();
 //import TabPanel from "./tabPanel.js";
 
-class App extends React.Component 
-{
-	constructor(props){
-		super(props)
-		this.state = {
-			scene: "intro",
-			src: "preintro.mp4",
-			sub: "intro.mp4",
-			dis: "none",
-			h: false
+function componentDidMount(){
+	
+	this.pclick = 
+		this.pclick.subscribe((e) => {
+		if(story.current.src == "default"){
+			story.next();
 		}
-	}
-
-	componentDidMount(){
-		window.video = this.video
-		window.video2 = this.video2
-
-		paper.setup(this.canvas)
-		let planet = new paper.Raster("./mercury.png")
-		window.planet = planet
-		let pclick = rx.Observable.fromEvent(planet, "click");
-
-		pclick.subscribe((e) => {
-			planet.remove();
-		});
-
-		planet.onMouseEnter = (e) => {
-			planet.opacity = 0.5
-		}
-
-		planet.onMouseLeave = (e) => {
-			planet.opacity = 1
-		}
-  		paper.view.setViewSize(window.innerWidth, window.innerHeight);
-
-		planet.position = paper.view.center;
-		this.video.addEventListener("ended", (e) => {
-
-			this.setState({dis: "block"});
-		});
-
-		window.addEventListener("resize", (e) => {
-			this.canvas.width  = window.innerWidth;
-  			this.canvas.height = window.innerHeight;
-  			paper.view.setViewSize(window.innerWidth, window.innerHeight);
-			planet.position = paper.view.center;
-
-			paper.view.draw();
-		});
-
-		this.video.addEventListener("timeupdate", () => {
-
-		//	if(this.video.currentTime >= 9.0) {
-		//		this.video.currentTime = 8.0;
-		//	}
-		});
-
-		//this.video.play();  
-
-		this.video2.play();  
-
-
-	}
-
-	componentDidUpdate(){
-		this.video.play();  
+		//planet.remove();
+	});
+		
+	planet.onMouseEnter = (e) => {
+		planet.opacity = 0.5
 	}
 	
-	render(){
-		return (
-			<div>
-				{ this.state.h ? "" : <video src={this.state.sub} ref={(ref) => this.video2 = ref}></video> }
-				<video src={_.find(story, ["scene", this.state.scene])["src"]} ref={(ref) => this.video = ref}></video>
-
-
-				<canvas id="drawSurf" className="draw" ref={(ref) => this.canvas = ref} resize="true"></canvas>
-			</div>
-			)
+	planet.onMouseLeave = (e) => {
+		planet.opacity = 1
 	}
+	
+	paper.view.setViewSize(window.innerWidth, window.innerHeight);
+	planet.position = paper.view.center;
+	console.log("WTF:", window, this);
+	
+
 }
 
 
+var canvas = null;
+var container = null;
+var planet = null;
 
-document.addEventListener( "DOMContentLoaded", () => {
-	ReactDOM.render(<App/>, document.getElementById('app-root'));
+var video = null;
+
+var tobjects = null;
+
+var resize = rx.Observable.fromEvent(window, "resize")
+	.map((e) => {return {height: window.innerHeight, width: window.innerWidth}})
+	.tap((size) => {
+		canvas.width = size.width
+		canvas.height = size.height
+		paper.view.setViewSize(size.width, size.height);
+	})
+
+resize.subscribe((size) => {
+	planet.position = paper.view.center;
+	paper.view.draw();
+})
+
+function end(){
+	story.next()
+	_.forEach(tobjects, (o) => o.remove())
+	video.remove()
+	video = show()
+
+}
+
+function show(){
+	let video = story.current.video
+	
+	let choices = story.choices()
+	if(choices.length!= 0){
+		console.log(choices)
+		var length = choices.length
+		tobjects = _.map(choices, (choice, n) => {
+			
+			let point = new paper.Point();
+			point.x = paper.view.center.x;
+			point.y = paper.view.center.y;
+
+
+			let coef = ((n+1)/(length/2 + 0.5))
+
+			point.x = point.x * coef
+			point.y = (point.y * 2) - 50
+			console.log(point)
+			let text = new paper.PointText({
+				point: point,
+				content: choice,
+				fillColor: 'white',
+				fontFamily: 'Courier New',
+				fontWeight: 'bold',
+				fontSize: 20,
+				justification: "center"
+			});
+			paper.view.draw();
+			text.onClick = () => window.next(n)
+			return text
+		})
+	}
+
+	window.video = video;
+	video.addEventListener("ended", end);
+
+	container.appendChild(video)
+	video.play()
+	return video
+
+}
+
+window.next = (args) => {
+	story.next(args)
+	video.remove()
+	_.forEach(tobjects, (o) => o.remove())
+	video = show()
+
+}
+
+window.addEventListener("load", (event) => {
+	console.log("Hello")
+
+	canvas = document.getElementById("drawSurf")
+	container = document.getElementById("container")
+
+	paper.setup(canvas)
+	planet = new paper.Raster("./mercury.png")
+	planet.position = paper.view.center;
+	planet.onClick = () => {
+		planet.remove()
+	}
+	video = show()
+
+
 }, false )
 
---
+++ src/js/story.js
@@ -0,0 +1,184 @@
+import _ from "lodash";
+
+var story = [
+	{
+		"scene": "intro",
+		"src": "vid/Intro.mp4",
+		"next": "conflict"
+	},	
+	{
+		"scene": "conflict",
+		"src": "vid/Background.mp4",
+		"choice": [
+			{ "show": "Покажи силата си", "scene": "provoke"},
+			{ "show": "Използвай думи", "scene": "use_words"},
+			{ "show": "Осави го", "scene": "provoke"}
+		]
+	},
+	{
+		"scene": "use_words",
+		"src": "vid/Background.mp4",
+		"choice": [
+			{ "show": "Докажи, че си по добър", "scene": "compete"},
+			{ "show": "Остави го да говори", "scene": ""},
+			{ "show": "Осави го", "scene": "provoke"}
+		]
+	},
+	{
+		"scene": "listen",
+		"src": "vid/Background.mp4",
+		"next": "tell_him_the truth"
+	},	
+	{
+		"scene": "provoke",
+		"src": "vid/Background.mp4",
+		"next": "compete"
+	},	
+	{
+		"scene": "compete",
+		"src": "vid/Background.mp4",
+		"choice": [
+			{ "show": "Механична силиа", "scene": "mech_force"},
+			{ "show": "Електропровидомост", "scene": "conductivity"},
+			{ "show": "Енергийна плътност", "scene": "electrical_density"}
+		]
+	},
+	{
+		"scene": "electrical_density",
+		"src": "vid/Cars.mp4",
+		"choice": [
+			{ "show": "Електропровидомост", "scene": "conductivity1"},
+			{ "show": "Механична силиа", "scene": "mech_force1"},
+		]
+	},
+
+	{
+		"scene": "conductivity",
+		"src": "vid/Phone.mp4",
+		"choice": [
+			{ "show": "Механична силиа", "scene": "mech_force3"},
+			{ "show": "Енергийна плътност", "scene": "electrical_density3"}
+		]
+	},
+	{
+		"scene": "mech_force",
+		"src": "vid/Meteor.mp4",
+		"choice": [
+			{ "show": "Електропровидомост", "scene": "conductivity2"},
+			{ "show": "Енергийна плътност", "scene": "electrical_density2"}
+		]
+	},
+	{
+		"scene": "electrical_density2",
+		"src": "vid/Cars.mp4",
+		"next": "conductivity4"
+	},	
+	{
+		"scene": "electrical_density3",
+		"src": "vid/Cars.mp4",
+		"next": "mech_force4"
+	},
+	{
+		"scene": "conductivity1",
+		"src": "vid/Phone.mp4",
+		"next": "conductivity4"
+	},	
+	{
+		"scene": "conductivity2",
+		"src": "vid/Phone.mp4",
+		"next": "mech_force4"
+	},
+	{
+		"scene": "mech_force1",
+		"src": "vid/Meteor.mp4",
+		"next": "conductivity4"
+	},	
+	{
+		"scene": "mech_force3",
+		"src": "vid/Meteor.mp4",
+		"next": "electrical_density4"
+	},
+	{
+		"scene": "electrical_density4",
+		"src": "vid/Cars.mp4",
+		"next": "end1"
+	},
+	{
+		"scene": "conductivity4",
+		"src": "vid/Phone.mp4",
+		"next": "end1"
+	},
+	{
+		"scene": "mech_force4",
+		"src": "vid/Meteor.mp4",
+		"next": "1nd1"
+	},
+	{
+		"src": "vid/Background.mp4",
+		"scene": "end1"
+	},
+	{
+		"src": "vid/Background.mp4",
+		"scene": "end2"
+	}	
+]
+window.s = story
+
+var cache = []
+
+function createVideo(element){
+	var item = _.find(cache,["src", element.src])
+	if(item != null){
+		element.video = item.video
+	}else{
+		let vid = document.createElement("video")
+		if(element.src == "vid/Background.mp4"){
+			vid.loop = true;
+		}
+		vid.src = element.src;
+		cache.push({src: element.src, video: vid})
+		element.video = vid;
+	}
+	
+}
+
+export default class Story {
+	constructor(){
+		this.story = story;
+		this.current = this.story[0];
+		_.forEach(this.story, createVideo)
+		console.log(cache)
+	}
+
+	scene(name){
+		return _.find(this.story, ["scene", name])
+	}
+
+	choices(){
+		return _.map(this.current.choice, "show");
+	}
+
+	neededVideos(){
+		if(this.current.choice){
+			return _.map(this.current.choice, (e) => this.scene(e.scene) )
+		}else{
+			return [ this.scene(this.current.next) ]
+		}
+	}
+
+	next(){
+		if(this.current.next){
+			this.current = this.scene(this.current.next);
+		}else{
+			if(arguments.length > 0 && this.current.choice){
+				if(arguments[0] <= this.current.choice.length){
+					this.current = this.scene(this.current.choice[arguments[0]].scene);
+				}else{
+					throw "This path does not exists"
+				}
+			}else{
+				throw "This transition to the next scene requires an argument"
+			}
+		}
+	}
+}
