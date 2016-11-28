3f9894e8aa77227c57e70555e287cce9dc06e84c
MrTroll420 <troll77bg@gmail.com>
2016-11-28 22:50:21 +0200

    Revert "package.bat added"
    This reverts commit 05d5a08e2be2541dc6f85806573c0030b75931fa.

--- src/package.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Are you sure you want to compile this for all OSs?
-pause
-electron-packager . --all --out ./publish/ --ignore=/js --ignore=/css --ignore=/node_modules --ignore=webpack.config.js
-pause
